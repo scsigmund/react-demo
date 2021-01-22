e81a8deb39fa6f62869136c8a891e11ea6f24b68
Sigmund <e92283@bswhealth.org>
2021-01-22 16:17:49 -0600

    update menu-items to nav

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,26 +12,31 @@ class Directory extends React.Component {
             sections: [{
                 title: 'hats',
                 imageUrl: 'https://cdn.pixabay.com/photo/2015/10/12/15/00/room-984076_960_720.jpg',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'jackets',
                 imageUrl: 'https://media.istockphoto.com/photos/scandinavian-farmhouse-living-room-interior-wall-mockup-picture-id1202848203',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },{
                 title: 'sneakers',
                 imageUrl: 'https://media.istockphoto.com/photos/retro-style-interior-background-picture-id1167055236',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },{
                 title: 'womens',
                 imageUrl: 'https://media.istockphoto.com/photos/modern-interior-of-living-room-concept-of-classic-blue-color-of-the-picture-id1193587033',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },{
                 title: 'mens',
                 imageUrl: 'https://media.istockphoto.com/photos/home-interior-mockup-with-green-sofa-rope-curtains-and-table-in-room-picture-id1045661884',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }
         ]}
     }
@@ -40,8 +45,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, size, id}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'));
